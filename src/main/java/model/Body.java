5bb37c9b62c1e97d8a0bf1fe414629de59371d71
mlv_ <levequemc@gmail.com>
2023-11-14 23:02:40 +0100

    feat: first commit. Send a serialized payload to the server and print the response.

---
+++ src/main/java/model/Body.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+
+@Builder
+public record Body(String prompt, Double temperature, List<ImageData> imageData) {
+	
+	@JsonProperty("image_data")
+	public List<ImageData> getImageData() {
+	    return imageData;
+	}
+}
---
+++ src/main/java/model/ImageData.java
@@ -0,0 +1,8 @@
+package model;
+
+import lombok.Builder;
+
+@Builder
+public record ImageData(String data, Integer id) {
+
+}
