8f2e084e5e25e6004901da478ff0e5202d768e2f
MarcLv <levequemc@gmail.com>
2024-05-13 13:57:28 +0800

    Linux gitlab build doesn't run but linux local build run with same command... testing a solution.

--- scripts/linux/PicturesAutoNamer.sh
+++ scripts/linux/PicturesAutoNamer.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-# Unset the JLINK_VM_OPTIONS variable
-unset JLINK_VM_OPTIONS
-
-# Set the DIR variable to the script's directory
-DIR="$(dirname "$0")"
-
-# Start javaw with the specified options and classpath
-javaw $JLINK_VM_OPTIONS -m fr.pan/fr.pan.main.Main "$@"
+#!/bin/sh
+JLINK_VM_OPTIONS=
+DIR=`dirname $0`
+$DIR/bin/java $JLINK_VM_OPTIONS -m fr.pan/fr.pan.main.Main "$@"
